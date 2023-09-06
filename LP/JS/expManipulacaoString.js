4a37f8ef00914f60b241574ba3734c05a5b205f9
Jorge Braga <jorgeabbraga@gmail.com>
2023-09-06 16:59:50 -0300

    Create expManipulacaoString.js
    Criação exemplo Manipulação de String

---
+++ LP/JS/expManipulacaoString.js
@@ -0,0 +1,9 @@
+const frase = "Exemplo de manipulação de strings em JavaScript";
+
+const parteDaFrase = frase.substring(11, 22); // Retorna a substring da posição 8 à 17 (inclusive)
+
+const fraseMaiuscula = frase.toUpperCase();
+
+console.log(parteDaFrase); //"manipulação"
+console.log(fraseMaiuscula);//"EXEMPLO DE MANIPULAÇÃO DE STRINGS EM JAVASCRIPT"
+
